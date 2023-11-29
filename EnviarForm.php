c2ac47b1cfb69ea32d8e30b48c62f789e2610dff
Oscar Ambrosio <114661997+theoscar23@users.noreply.github.com>
2023-11-29 15:32:21 +0100

    Enviar Datos por PHP

---
+++ EnviarForm.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recoger datos del formulario
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $direccion = $_POST["direccion"];
+    $email = $_POST["email"];
+    
+
+    // Configuración del correo
+    $para = "oscarambrosio23@gmail.com.com";
+    $asunto = "Nuevo mensaje de contacto";
+
+    // Contenido del correo
+    $contenido = "Nombre: $nombre\n";
+    $contenido = "Apellido: $apellido\n";
+    $direccion = "Direccion: $direccion\n";
+    $contenido .= "Correo electrónico: $email\n\n";
+    
+
+    // Enviar el correo
+    mail($para, $asunto, $contenido);
+
+    // Redireccionar o mostrar un mensaje de éxito
+    $mensaje="Gracias por contactar con nosotros"
+    header("Location: index.html");
+    echo"<p>$mensaje</p>";
+} else {
+    // Acceso directo al script, redirigir a la página de inicio
+    header("Location: index.html");
+    echo"<p>$mensaje</p>";
+}
+?>
